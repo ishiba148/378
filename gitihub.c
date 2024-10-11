16ade6cd51376dfe44c3b8745baaeb39a790f6db
ishiba148 <t236053@st.yamagata-u.ac.jp>
2024-10-11 18:48:00 +0900

    Create gitihub.c

---
+++ gitihub.c
@@ -0,0 +1,3 @@
+;lzisdufclzkj;dfliha;ueg:otia]ogj;zhioih;g :nvoket:jtagzgadgfaeg;alieg:aigrr:agmsdjfsah;l
+:laskdajfakjsdkfjls.akd.kasjdshfjk.sjg.sjafhjash.;jah/akl/ahkl/hakhglaih/lghil/arinlg
+  a;lkgh;alierhlh/ialgi/hr/laihgliahglh;ril/alehi:a;jdj:;gjaorhgaetagragrrarjgjar;\gjoa
