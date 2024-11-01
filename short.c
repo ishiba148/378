dd65b915141438e0e1c2217396a4496aaf241c0b
ishiba148 <t236053@st.yamagata-u.ac.jp>
2024-11-01 19:40:10 +0900

    Create short.c

---
+++ short.c
@@ -0,0 +1,14 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("short umaximum:%d\n", USHRT_MAX);
+    printf("short maximum:%d\n", SHRT_MAX);
+    printf("short minimum:%d\n", SHRT_MIN);
+    
+    return 0;
+    
+}
